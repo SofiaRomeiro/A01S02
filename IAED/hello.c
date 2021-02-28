8210fbf2ad3c332e8c9f3c377393bddf930889f3
Sofia Romeiro <sofiaromeiro23@gmail.com>
2021-02-28 22:53:38 +0000

    hello world

---
+++ IAED/hello.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(int argc, char const *argv[]) {
+
+    printf("Hello World");
+
+    int num = 999;
+    printf("%d \n", num);
+
+    printf("argc: %d", argc);
+    printf("argv: %s", argv[1]);
+    return 0;
+}
