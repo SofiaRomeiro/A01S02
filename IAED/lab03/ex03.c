5409d1dd7bed03e21273be76a00f42700b330fe9
Sofia Romeiro <sofiaromeiro@localhost.localdomain>
2021-03-15 16:09:54 +0000

    updates

--- IAED/lab03/ex03.c
+++ IAED/lab03/ex03.c
@@ -3,41 +3,41 @@
 
 void cruz(int n) {
 
-      int lc;
+      int line, pos;
 
     /* pair num */
     if (n%2 == 0) {
-        for (lc=1; lc < 5; ++lc) {
-            if (lc == 1) {
-                printf("* - - -\n");
+        for (line=1; line < 9; ++line) {
+
+            for (pos=1; pos < 9; ++pos) {
+
+                printf("\n%d -> pos\n", pos);
+
+
+                if (pos==line || (pos == (n-line) + 1)) {
+                    if (pos != 8) {
+                        printf(" * ");
+                    }
+                    else {
+                        printf(" * \n");
+                    }
+                    
+                }
+                else {
+                    if (pos != 8) {
+                        printf(" - ");
+                    }
+                    else {
+                        printf(" - \n");
+                    }
+                    
+                }
             }
-            else if (lc == 2) {
-                printf("- * - -\n");
-            }
-            else if (lc == 3) {
-                printf("- - * -\n");
-            }
-            else {
-                printf("- - - *\n");
-            }
+         
         }
 
-        for (lc=1; lc < 5; ++lc) {
-            if (lc == 1) {
-                printf("* - - -\n");
-            }
-            else if (lc == 2) {
-                printf("- * - -\n");
-            }
-            else if (lc == 3) {
-                printf("- - * -\n");
-            }
-            else {
-                printf("- - - *\n");
-            }
-        }
+ 
     }
-
     /* impair num */
     else {
 
