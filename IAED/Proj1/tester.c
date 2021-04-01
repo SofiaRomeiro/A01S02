f8286ccedd4affb78ed592374599b8f1facf2d8c
SofiaRomeiro <sofiaromeiro23@tecnico.ulisboa.pt>
2021-04-01 17:27:27 +0100

    updates

--- IAED/Proj1/tester.c
+++ IAED/Proj1/tester.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 typedef struct {
 	char name[10];
@@ -19,7 +20,6 @@ Cao leCao() {
 	while ((c=getchar()) != EOF && c != '\n') {
 		if (c >= '0' && c <= '9') {
 			cao.age = cao.age * 10 + (c - '0');
-			printf("age : %d\n", cao.age);
 		} 
 	}
 	return cao;
@@ -31,11 +31,17 @@ void escreveCao(Cao cao, int i){
 
 int main() {
 	int i;
-	Cao caes[10];
+	Cao caes[3];
+	char name[4] = "rui";
 
 	for (i=0; i < 3; i++) {
 		caes[i] = leCao();
-		escreveCao(caes[i], i);
+	}
+
+	for (i=0; i < 3 ; i++) {
+		if (!strcmp(caes[i].name, name)) {
+			printf("nome : %s\n", caes[i].name);
+		}
 	}
 
 	return 0;
