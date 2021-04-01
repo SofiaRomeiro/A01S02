c5340f352fcba38041bb52f29e80bfa3bfd932e8
Sofia Romeiro <sofia@kali.kali.com>
2021-04-01 17:29:39 +0100

    updates

--- IAED/Proj1/tester.c
+++ IAED/Proj1/tester.c
@@ -1,54 +1,13 @@
 #include <stdio.h>
-#include <stdlib.h>
-
-#define GLOBALMAX 51
-
-int readnum(int duration) {
-	char c;
-	while ((c = getchar()) != EOF && c != '\n' && c != ' ') {
-		if (c >= '0' && c <= '9') {
-			duration = duration * 10 + (c - '0');
-		}
-		else {
-			printf("Please insert a positive number\n");
-			exit(EXIT_FAILURE);
-		}
-	}
-	return duration;
-}
-
-/* l [<id> <id> ...] */
-/* l 1 3 10 200 */
-
-void readlist(int list[], int maxsize) {
-
-	char c;
-	int id=0, i, idpos=0;
-
-	for (i=0; i < maxsize && (c=getchar()) != EOF && c != '\n'; i++) {
-		/* se c for ' ' -> passa a frente, adiciona o id ao array e da clean no id*/
-		if (c == ' ') {
-			list[idpos++] = id;
-			id = 0;			
-		}
-		else {			
-			if (c >= '0' && c <= '9') {
-				id = id * 10 + (c - '0');
-			}
-		} 			
-	}
-	list[idpos] = id;		
-} 
 
 int main() {
+	int i=2, j = 3; 
+	float res=1.0;
 
-	int duration = 0;
-	int list[20];
+	/*dividir com resultado inteiro : */
+	res = i / (float) j;
 
-	printf("?\n");
-	printf("%d : duration\n", readnum(duration));
-	readlist(list, 10000);
-
+	printf("%f", res);
+	
 	return 0;
 }
-
