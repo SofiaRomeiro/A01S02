b2c17feb9f131376f4371e498be13d4473cdd6dd
SofiaRomeiro <sofiaromeiro23@tecnico.ulisboa.pt>
2021-05-10 16:30:22 +0100

    updates

--- IAED/Proj2/try/little_experiences.c
+++ IAED/Proj2/try/little_experiences.c
@@ -1,70 +1,107 @@
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
 
-int main() {
-    
-    char *name = NULL;
-    char *message = NULL; 
+typedef struct dir {
+    char *name;
+    struct dir *next;
+} *directory;
 
-    char number[10];
-    char buffer_message[1000];
-    char buffer_name[1000];
-    char c;
-    int r_name=0, r_number=0, r_message=0;
-    int i=0, j=0, k=0;
+directory constructor() {
+    directory new_dir = (directory) malloc(sizeof(struct dir));
+    new_dir->name = NULL;
+    new_dir->next = NULL;
 
-    while((c=getchar()) != EOF && c != '\n') {
+    return new_dir;
+}
 
-        if (c != ' ' && !r_number) {
-            number[i++] = c;
-        }
+void destructor(directory dir) {
 
-        else if (c == ' ' && !r_number) {
-            number[i]='\0';
-            r_number = 1;
-        }
+    directory current, next;
+    for (current = dir; current != NULL; current = next) {
+        next = current->next;
+        free(current->next);
+        free(current);
+    }
+}
 
-        else if (c == ' ' && !r_message) {
-            continue;
-        }
-        
-        else if (c != ' ' && !r_name) {            
-            buffer_name[j++] = c;
-        }
+void push(char *dir, directory head) {
+    directory new;
 
-        else if (c == ' ' && !r_name) {
-            r_name = 1;
+    new = (directory) malloc(sizeof(struct dir));
+    new->name = (char *) malloc(sizeof(char)*(strlen(dir)+1));
+    strcpy(new->name, dir);
+    new->next = head;
+    head = new;
+}
+
+directory insertBegin(directory head, char buffer[]) {
+    directory new = (directory) malloc(sizeof(struct dir));
+    new->name = (char *) malloc(sizeof(char)* (strlen(buffer)+1));
+    strcpy(new->name, buffer);
+    new->next = head;
+    /* new will be the current head */
+    return new;
+}
+
+directory insertEnd(directory head, char buffer[]){
+    directory temp;
+    directory new = (directory) malloc(sizeof(struct dir));
+    new->name = (char *) malloc(sizeof(char)* (strlen(buffer)+1));
+    strcpy(new->name, buffer);
+    new->next = NULL;
+    if(head == NULL) return new;
+    for(temp = head; temp->next != NULL; temp = temp->next)
+    ;
+    temp->next = new;
+    return head;
+}
+
+directory newPath(char path[], directory head) {
+    int i, j=0, len = strlen(path);  
+    char c,buffer[20];
+    directory new;
+
+
+
+    for (i=0; i <= len; i++) {
+        if ((c = path[i]) == '/' || c == ' ' || c == '\0') {
+
+            if (strlen(buffer) > 1) {
+                buffer[j] = '\0';
+                j = 0;
+                new = insertEnd(head, buffer);
+                head = new;
+                buffer[0] = '\0';                
+            }
         }
 
         else {
-            r_message=1;
-            buffer_message[k++] = c;
+            buffer[j++] = path[i];
         }
     }
-    
-    buffer_name[j] = '\0';
-    buffer_message[k] = '\0';  
 
-      
-    if (buffer_name[0] != '\0') {
-        name = (char *)malloc(sizeof(char)*(strlen(buffer_name)+1));
-        strcpy(name, buffer_name);
+    return head;
+} 
+
+
+
+int main() {
+
+    char path[100];
+
+    directory head = constructor();
+    directory copyhead;
+
+    strcpy(path, "/usr/local/bin/ncinfo/voadores");    
+
+    copyhead = newPath(path, head);
+
+    for (copyhead=copyhead->next; copyhead != NULL; copyhead = copyhead->next) {
+        printf("%s.\n", copyhead->name);
     }
 
-    if (buffer_message[0] != '\0') {
-        message = (char *)malloc(sizeof(char)*(strlen(buffer_message)+1));
-        strcpy(message, buffer_message);
-    }
+    destructor(copyhead);
 
-    printf("%s, %s, %s\n", number, name, message);
-
-
-
-
-
-    
-
-    
     return 0;
 }
