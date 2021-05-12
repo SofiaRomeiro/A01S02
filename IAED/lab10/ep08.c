9f5ae8d77df70d4fb01e997244c0e9c76fa7a156
SofiaRomeiro <sofiaromeiro23@tecnico.ulisboa.pt>
2021-05-12 15:16:22 +0100

    updates

--- IAED/lab10/ep08.c
+++ IAED/lab10/ep08.c
@@ -13,23 +13,71 @@ pos  |   values
   6  |  15
   7  |     X
   8  |     X
+
 */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 #define MAX 10
 
 typedef struct next {
     int value;
     struct next *next;
-} Next;
+} *Next;
 
-Next *hash[MAX];
+Next *hash=NULL;
 
 int hashing(int val) { return val % MAX; }
 
 int maxcollision() {
 
+  int max=0, iter=0, i;
+  Next aux;
 
-
-    return ;
+  while (iter < MAX) {
+    aux = hash[iter];
+    for (i=0; aux != NULL; aux = aux->next, i++);
+    if (i > max) {
+      max = i;
+    }
+    iter++;
+  }
+  
+  return max-1;
 }
+
+
+Next insertBegin(Next head, int a) {
+  Next new = (Next)malloc(sizeof(struct next));
+  new->value = a;
+  new->next = head;
+  return new;
+}
+
+void Init() {
+  int i;
+  hash = (Next *)malloc(MAX * sizeof(struct next));
+  for (i = 0; i < MAX; i++) hash[i] = NULL;
+}
+
+void STinsert(int num) {
+  int i = hashing(num);
+  hash[i] = insertBegin(hash[i], num);
+}
+
+int main() {
+
+  int i;
+  Init();
+
+  while (scanf("%d\n", &i)) {
+    STinsert(i);
+  }
+
+  printf("max = %d\n", maxcollision());
+  return 0;
+}
+
 
